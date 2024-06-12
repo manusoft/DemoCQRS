c9d00cf6b8315183488624dec929edbf2aa89903
manoj@office <alconsoft@gmail.com>
2024-06-12 12:48:36 +0400

    Add get movie by id query and handler.

---
+++ DemoCQRS/Features/Movies/Queries/GetMovieByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using DemoCQRS.Data;
+using MediatR;
+
+namespace DemoCQRS.Features.Movies.Queries;
+
+public sealed record GetMovieByIdQuery(int Id): IRequest<Movie?>;
---
+++ DemoCQRS/Features/Movies/Queries/GetMoviePlayerByIdQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using DemoCQRS.Data;
+using MediatR;
+
+namespace DemoCQRS.Features.Movies.Queries;
+
+public sealed class GetMoviePlayerByIdQueryHandler : IRequestHandler<GetMovieByIdQuery, Movie?>
+{
+    private readonly AppDbContext _context;
+
+    public GetMoviePlayerByIdQueryHandler(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Movie?> Handle(GetMovieByIdQuery request, CancellationToken cancellationToken)
+    {
+        var movie = await _context.Movies.FindAsync(request.Id);
+        return (movie);
+    }
+}
